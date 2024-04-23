c26e2137dcbe3bca8391f1d484accbcd0223017b
DanieleBernassola <danielebernassola02@gmail.com>
2024-04-23 15:42:36 +0200

    [UPD] Added prompts for LastName and FavColor

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ let currentElement = document.getElementById('info');
 let currentText = currentElement.innerHTML;
 
 const myName = prompt('Inserisci il tuo nome');
+const myLastName = prompt('Inserisci il tuo cognome');
+const myFavColor = prompt('Inserisci il tuo colore preferito');
 
-currentText = `${myName}`;
+currentText = `${myName}${myLastName}${myFavColor}21`;
 
 currentElement.innerHTML = currentText;
