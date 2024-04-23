b37ebcabeb8b24ed158b3c9decb69b31c312b51a
DanieleBernassola <danielebernassola02@gmail.com>
2024-04-23 15:53:51 +0200

    [UPD BONUS2] Added randomNumber

--- js/main.js
+++ js/main.js
@@ -5,8 +5,9 @@ let currentText = currentElement.innerHTML;
 const myName = prompt('Inserisci il tuo nome');
 const myLastName = prompt('Inserisci il tuo cognome');
 const myFavColor = prompt('Inserisci il tuo colore preferito');
+const randomNumber = Math.random() * 10;
 
-currentText = `${myName}${myLastName}${myFavColor}21`;
+currentText = `${myName}${myLastName}${myFavColor}${randomNumber}`;
 
 console.log(currentText);
 
